eb802e061a5aa78e7ddae5ba44730d24ff60e3b8
Mijon Utomi <Mijon.utomi@gmail.com>
2020-01-22 14:06:39 -0600

    metod overloading excerise

---
+++ MOE/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MOE
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine();
+        }
+
+        public static int Add(int x, int y)
+        {
+            return (x + y);
+
+        }
+        public static decimal Add(decimal x, decimal y)
+        {
+            return (x + y);
+        }
+        public static string Add(int x, int y, bool shouldAdd)
+        {
+            if (shouldAdd)
+                x += y;
+
+            string dollars = $"{x}dollar";
+            if (x > 1)
+                dollars += "s";
+            return dollars;
+        }
+    }
+}
